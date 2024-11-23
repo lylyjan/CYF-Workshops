3dbf2dd798c58b9eec6e795192f3204fa3b0714f
Ly-Ly Janusz <153077816+lylyjan@users.noreply.github.com>
2024-11-23 12:06:50 +0100

    solved kata

--- objects/200.js
+++ objects/200.js
@@ -1,14 +1,15 @@
 // Predict and explain...
 // What will happen when this program is run?
-// Check your prediction and explanation by running the program.
+// Check your prediction and explanation by running the program. there are some syntax problems, after they are fixed it runs if
+// grep 'assert' 200.js -C 3 
 // Fix anything that needs fixing.
 
 const person = {
     "name": "Jemima",
-    "location",
-    "id_number" = 9,
+    "location": "",
+    "id_number" :9,
 };
 
-console.assert(person.name === "Jemima");
-console.assert(person.location === "Glasgow");
-console.assert(person.id_number === 9);
+console.assert(person.name === "Jemima");//true
+console.assert(person.location === "Glasgow");//false
+console.assert(person.id_number === 9);//true
